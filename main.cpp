6b4ae52b943196e929646fbc903465b93fa1a775
hzh <3200104436@zju.edu.cn>
2022-11-24 00:27:57 +0800

    搭建了拟牛顿法的框架

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ using namespace std;
 
 // 超参数
 #define LAMDA 1
-// #define LR 0.15  for GD
-#define LR 0.000000000000000001
-#define ITERS 600
+// #define LR 0.15  //for GD
+#define LR 0.00000000001
+#define ITERS 2000
 
 
 class Matrix{
@@ -213,7 +213,7 @@ Matrix& operator /( const Matrix &a, const Matrix &b){
         Matrix* new_mat;
 
         if( (b.get_col()!=a.get_col() || b.get_row()!=a.get_row()) && 
-            !((b.get_row()==1&&b.get_col()==1||a.get_row()==1&&a.get_col()==1))
+            !(b.get_row()==1&&b.get_col()==1)
         ){
             cout << ">>>>" <<b.get_col()<<"  "<<a.get_col()<<endl;
             cout <<"can't mul with different shape!"<<endl;
@@ -225,11 +225,6 @@ Matrix& operator /( const Matrix &a, const Matrix &b){
             new_mat = new Matrix(a.get_row(),a.get_col());
             for(int i=0;i<a.get_row();i++)
                 for(int j=0;j<a.get_col();j++)
-                    (*new_mat)[i][j]=a[0][0] / b[i][j];
-        }else if(a.get_row()==1&&a.get_col()==1){
-            new_mat = new Matrix(b.get_row(), b.get_col());
-            for(int i=0;i<b.get_row();i++)
-                for(int j=0;j<b.get_col();j++)
                     (*new_mat)[i][j]=a[i][j] / b[0][0];
         }else{
             new_mat = new Matrix(b.get_row(), b.get_col());
@@ -328,23 +323,22 @@ public:
 };
 
 
-// 梯度下降
-class GD {
+class MethodBase{
+protected:
     Matrix* w, *y, *input;
     int d;
     double lamda=LAMDA;
     double lr=LR;
     Dataloader loader;
 public:
-    //构造函数
-    GD(int d, Matrix& input, Matrix& y):d(d){
+    MethodBase(int d, Matrix& input, Matrix& y):d(d){
         this->w = new Matrix(1,d);
         this->input = new Matrix(input);
         this->y = new Matrix(y);
         (*w)[0][0]=0.1;
     }
-    //构造函数,读文件输入
-    GD(const char* filename){
+
+    MethodBase(const char* filename){
         loader.load_data(filename);
         this->d = loader.get_d();
         this->w = new Matrix(1,d);
@@ -360,9 +354,9 @@ public:
         for(int i=0;i<d;i++){
             result+=*(a+i) * *(b+i);
         }
-        // cout << *a << " " << *b << endl;
         return result;
     }
+
     //计算梯度
     Matrix& get_gradient(Matrix* ground_truth, Matrix* input){
         Matrix gradient(1,d);
@@ -370,16 +364,6 @@ public:
             gradient += Matrix((*input)[i], input->get_col()) * Matrix( ((*ground_truth)[0][i] - vector_inner_product((*w)[0],(*input)[i], this->d)) ); // 
         }
         return -2*gradient + lamda*(*w);
-    }
-    // 使用梯度下降更新一步参数
-    void update_step(){
-        Matrix gradient(get_gradient(y, input));
-        // cout << "grad:";
-        // gradient.show();
-        (*w)-= 1/(double)input->get_row() * lr*gradient;
-        // cout << "new w:";
-        // w->show();
-        show_loss();
     }
 
     //输出loss
@@ -392,6 +376,41 @@ public:
         loss += lamda*w->norm();
         loss /= input->get_row();
         cout << "loss: "<< loss <<endl;
+    }
+    //返回loss
+    double get_loss(){
+        double loss=0;
+        for(int i=0;i<input->get_row();i++){
+            Matrix tmp =  Matrix( ((*y)[0][i] - vector_inner_product((*w)[0],(*input)[i], this->d)) );
+            loss += tmp[0][0]*tmp[0][0];
+        }
+        loss += lamda*w->norm();
+        loss /= input->get_row();
+        return loss;
+    }
+
+    void show_result(){
+        cout << "result{" << endl << "parameter: ";
+        w->show();
+        cout <<"loss: " << get_loss() << endl;
+        cout <<"}"<<endl;
+    }
+};
+
+
+// 梯度下降
+class GD : public MethodBase{
+public:
+    //构造函数
+    GD(int d, Matrix& input, Matrix& y):MethodBase(d, input, y){}
+    //构造函数,读文件输入
+    GD(const char* filename):MethodBase(filename){}
+
+    // 使用梯度下降更新一步参数
+    void update_step(){
+        Matrix gradient(get_gradient(y, input));
+        (*w)-= 1/(double)input->get_row() * lr*gradient;
+        show_loss();
     }
 };
 
@@ -427,45 +446,15 @@ Matrix& matmul( Matrix& a, Matrix&b){
 // 共轭梯度下降
 // 在非ridge回归模型中，回归系数正是线性方程组Ax=b的解，其中A=X′X，b=X′y
 // 在ridge回归模型中，回归系数正是线性方程组Ax=b的解，其中A=X′X+(1/2)*lamda，b=X′y
-class CD{
-Matrix* w, *y, *input;
-    int d;
-    double lamda=LAMDA;
-    double lr=LR;
-    Dataloader loader;
+class CD : public MethodBase{
 public:
     //构造函数
-    CD(int d, Matrix& input, Matrix& y):d(d){
-        this->w = new Matrix(1,d);
-        this->input = new Matrix(input);
-        this->y = new Matrix(y);
-        (*w)[0][0]=0.1;
-    }
+    CD(int d, Matrix& input, Matrix& y):MethodBase(d, input, y){}
     //构造函数,读文件输入
-    CD(const char* filename){
-        loader.load_data(filename);
-        this->d = loader.get_d();
-        this->w = new Matrix(1,d);
-        this->input = new Matrix(loader.get_input_matrix());
-        this->y = new Matrix(loader.get_ground_truth());
-
-        (*w)[0][0]=0.1;
-    }
-
-    //计算内积
-    double vector_inner_product(double* a, double *b, int d){
-        double result=0;
-        for(int i=0;i<d;i++){
-            result+=*(a+i) * *(b+i);
-        }
-        // cout << *a << " " << *b << endl;
-        return result;
-    }
-
+    CD(const char* filename):MethodBase(filename){}
     // 迭代更新参数
     // input×w=y
     void update(){
-        
         Matrix A = Matrix( matmul((*input).T(), (*input)) );
         Matrix lamda_mat = Matrix(d,d);
         cout << lamda << endl;
@@ -482,6 +471,7 @@ public:
             a = matmul(r.T(),r)/matmul(matmul(p.T(), A), p);
             x += matmul(a,p);
             Matrix next_r = r - matmul(matmul(a,A), p);
+            // 将原本的r小条件改为loss小条件也可执行，由于r后期十分小
             // if r is small exit, I change the condition to if loss is small
             beta = matmul(next_r.T(),next_r)/matmul(r.T(),r);
             p = next_r + matmul(beta, p);
@@ -494,26 +484,51 @@ public:
         }
     }
 
-    //输出loss
-    void show_loss(){
-        double loss=0;
-        for(int i=0;i<input->get_row();i++){
-            Matrix tmp =  Matrix( ((*y)[0][i] - vector_inner_product((*w)[0],(*input)[i], this->d)) );
-            loss += tmp[0][0]*tmp[0][0];
-        }
-        loss += lamda*w->norm();
-        loss /= input->get_row();
-        cout << "loss: "<< loss <<endl;
+};
+
+// 拟牛顿法
+class QuasiNewton: public MethodBase{
+
+public:
+    //构造函数
+    QuasiNewton(int d, Matrix& input, Matrix& y):MethodBase(d, input, y){
+        
     }
-    double get_loss(){
-        double loss=0;
-        for(int i=0;i<input->get_row();i++){
-            Matrix tmp =  Matrix( ((*y)[0][i] - vector_inner_product((*w)[0],(*input)[i], this->d)) );
-            loss += tmp[0][0]*tmp[0][0];
+    //构造函数,读文件输入
+    QuasiNewton(const char* filename):MethodBase(filename){
+        // 参考参数(由GD得 -0.415388 4.4534 5.51199 4.64595 4.90556 -15.4694 -2.40434 11.9906 4.58096)
+        double a[9]={-0.415388, 4.4534, 5.51199, 4.64595, 4.90556, -15.4694, -2.40434, 11.9906, 4.58096};
+        delete w;
+        w = new Matrix(a,9);
+    }
+    // 使用梯度下降更新一步参数
+    void quasi_newton_iter(){
+        Matrix G = Matrix(input->get_col(), input->get_col());
+        Matrix delta = Matrix(w->get_row(), w->get_col());
+        Matrix A = Matrix(*input);
+        // Matrix x = Matrix(*w);
+        Matrix g = 1/(double)input->get_row() * get_gradient(y, &A).T();
+        Matrix delta_g = Matrix(g.get_row(), g.get_col());
+        for(int i=0; i<input->get_col();i++)
+            G[i][i]=1;
+        for(int i=0;i<4;i++){ //while(g.norm()>1)
+            show_loss();
+            G.show();
+            delta = matmul(G,g);
+            // 此处的lr可以通过搜索步长得出
+            cout <<"vvvv"<<endl;
+            // w->show();
+            // delta.T().show(); // very big
+            cout <<"!!!!";
+            G.show();
+            (*w) += LR * delta.T(); 
+            // w->show();
+            cout <<"^^^^"<<endl;
+            delta_g = get_gradient(y, &A).T() - g;
+            g = get_gradient(y, &A).T();
+            G += matmul(delta,delta.T())/matmul(delta.T(),delta_g) - matmul(matmul(G,delta_g), matmul(delta_g.T(), G))/matmul(matmul(delta_g.T(),G), delta_g);
+            show_loss();
         }
-        loss += lamda*w->norm();
-        loss /= input->get_row();
-        return loss;
     }
 };
 
@@ -527,9 +542,14 @@ int main(){
     //     cout <<"iters:   " << i;
     //     calculer.update_step();
     // }
-    
     // CD
-    CD calculer = CD("./datasets/abalone.txt");
-    calculer.update();
+    // CD calculer = CD("./datasets/abalone.txt");
+    // calculer.update();
+
+    // QuasiNewton
+    QuasiNewton calculer = QuasiNewton("./datasets/abalone.txt");
+    calculer.quasi_newton_iter();
+
+    calculer.show_result();
 }
 
